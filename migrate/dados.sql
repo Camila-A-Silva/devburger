69d58375d3809a9b414991f87c4ff53dabd287b0
Camila Silva <camila.a.silva19@aluno.senai.br>
2026-04-10 11:28:01 -0300

    ADD: acrescentando itens ao bd

--- migrate/dados.sql
+++ migrate/dados.sql
@@ -1,33 +1,47 @@
 INSERT INTO `db_hamburguer`.`produto`
 (`produtos`,
 `descricao`,
+`destaque`,
 `preco`,
-`url_imagem`)
+`url_imagem`,
+`disponibilidade`)
 VALUES
 ("Classic Dev",
 "Pão brioche, carne suculenta e queijo derretido.",
+"1",
 "25",
-"https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&w=600"
+"https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&w=600",
+"1"
 ),
 ("Double Stack",
 "Dois hambúrgueres, bacon crocante e molho especial.",
+"1",
 "38",
-"https://images.pexels.com/photos/2983101/pexels-photo-2983101.jpeg?auto=compress&cs=tinysrgb&w=600"
+"https://images.pexels.com/photos/2983101/pexels-photo-2983101.jpeg?auto=compress&cs=tinysrgb&w=600",
+"1"
 ),
 ("Veggie Script",
 "Hambúrguer de grão de bico com salada fresca.",
+"1",
 "30",
-"https://images.pexels.com/photos/3219483/pexels-photo-3219483.jpeg?auto=compress&cs=tinysrgb&w=600"
+"https://images.pexels.com/photos/3219483/pexels-photo-3219483.jpeg?auto=compress&cs=tinysrgb&w=600",
+"1"
 ),
 ("Java Chicken",
 "Frango empanado crocante com alface e maionese.",
+"1",
 "28",
-"https://images.pexels.com/photos/12034622/pexels-photo-12034622.jpeg"),
+"https://images.pexels.com/photos/12034622/pexels-photo-12034622.jpeg",
+"1"),
 ("Python Onion",
 "Anéis de cebola, barbecue e queijo cheddar.",
+"1",
 "33",
-"https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&w=600"),
+"https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&w=600",
+"1"),
 ("React Salad",
 "Uma opção leve e reativa para o seu almoço.",
+"1",
 "27",
-"https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg?auto=compress&cs=tinysrgb&w=600");
+"https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg?auto=compress&cs=tinysrgb&w=600",
+"1");
