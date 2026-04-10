10cb025230e6a2cc906786cf85d9a152454dd6a8
Camila Silva <camila.a.silva19@aluno.senai.br>
2026-04-10 11:26:30 -0300

    ADD: criando as rotas e páginas para cada produto

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from model.produto import recuperar_produtos, recuperar_produtos_destaque
+from model.produto import recuperar_produtos, recuperar_produtos_destaque, recup_produto
 
 app = Flask(__name__)
 
@@ -9,6 +9,11 @@ def pg_inicial():
     destaque = recuperar_produtos_destaque()
     return render_template("index.html", produto = produto, destaque = destaque)
 
+
+@app.route("/produto/<codigo>")
+def pg_produto(codigo):
+    prod = recup_produto(codigo)
+    return render_template("produto.html", prod = prod)
 
 if (__name__) == "__main__":
     app.run(debug=True)
--- model/produto.py
+++ model/produto.py
@@ -11,8 +11,16 @@ def recuperar_produtos():
 
 def recuperar_produtos_destaque():
     conexao, cursor = conectar()
-    cursor.execute("SELECT codigo, produtos, descricao, destaque, preco, url_imagem, disponibilidade from produto WHERE destaque = 1")
+    cursor.execute("SELECT codigo, produtos, url_imagem from produto WHERE destaque = 1")
 
     produto = cursor.fetchall()
+    conexao.close()
+    return produto
+
+def recup_produto(codigo:int):
+    conexao, cursor = conectar()
+    cursor.execute("SELECT codigo, produtos, descricao, destaque, preco, url_imagem, disponibilidade from produto WHERE codigo = %s",[codigo])
+
+    produto = cursor.fetchone()
     conexao.close()
     return produto
